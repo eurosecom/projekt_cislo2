c4f77f24d7d9662ff87b6da8c1e6ee758bdcba34
eurosecom <eurosecom1@gmail.com>
2014-08-25 21:49:52 +0200

    Kopiruj na remote
    Nakopirovanie projektu na remote

---
+++ src/com/eusecom/testujdrive/StatistikaActivity.java
@@ -0,0 +1,22 @@
+package com.eusecom.testujdrive;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class StatistikaActivity extends Activity{
+    
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.statistics2);
+        
+        
+        
+    }
+    //koniec oncreate
+     
+   
+}
